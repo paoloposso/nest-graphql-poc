bd08ff71a2f346a11d92a65c71bd9797c6b5b691
Paolo Posso <pvictorsys@gmail.com>
2022-05-29 19:27:42 -0400

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthorsModule } from './authors/authors.module';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
 
 @Module({
   imports: [
+    AuthorsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      // transformSchema: schema => upperDirectiveTransformer(schema, 'upper'),
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
     }),
   ],
 })
---
+++ src/authors/authors.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { PostsService } from "src/posts/posts.service";
+import { AuthorsResolver } from "./authors.resolver";
+import { AuthorsService } from "./authors.service";
+
+@Module({
+    providers: [AuthorsResolver, AuthorsService, PostsService],
+})
+export class AuthorsModule {}
+  
