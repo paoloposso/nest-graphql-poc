3f27e73e79319a0a9ad090f998769c32a6fb3003
Paolo Posso <pvictorsys@gmail.com>
2022-06-12 22:41:16 -0300

    remove unused files

--- src/authors/authors.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { PostsService } from "src/authors/posts.service";
-import { AuthorsResolver } from "./authors.resolver";
-import { AuthorsService } from "./authors.service";
-
-@Module({
-    providers: [ AuthorsResolver, AuthorsService, PostsService],
-})
-export class AuthorsModule {}
